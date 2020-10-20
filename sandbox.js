c101a2e38cd9480e6a880fae29cf56315bd54424
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-20 20:43:24 +0200

    Added event listeners for subcont 1 and 2

--- sandbox.js
+++ sandbox.js
@@ -18,4 +18,31 @@ window.onload = function(){
        });
    });
 
+   // Adding event listeners for first and second subcontainer
+   button1.addEventListener("click", function(e){
+
+        e.preventDefault();
+        if(inputQuery1.value > 0){
+            var number = inputQuery1.value * 0.45359237;
+            var n = number.toFixed(2);
+            output1.textContent = inputQuery1.value + " lbs" + " = " + n + " kg";
+        } else{
+            alert("Only values larger than 0!");
+        }
+
+    });
+
+    button2.addEventListener("click", function(e){
+
+        e.preventDefault();
+        if(inputQuery2.value > 0){
+            var number = inputQuery2.value / 0.45359237;
+            var n = number.toFixed(2);
+            output2.textContent = inputQuery2.value + " kg" + " = " + n + " lbs";
+        } else{
+            alert("Only values larger than 0!");
+        }
+
+    });
+
 }
